47768e156cefe9627d09c12acfa1dc44f344d95d
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-03-09 19:15:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ bot.on('guildMemberAdd', member => {
 bot.on("message", async message => {
     if(message.author.bot) return;
     if(message.channel.type === "dm") return;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
   if (message.content === '^help') {
     message.channel.send("DMed you! Check it out for all the info!")
     return message.author.send("**My Commands:** *all commands start with `^` prefix.*\n\t`help` shows this message.\n\t`test` tests to see if the bot is properly set up.\n\t`info` shows bot info.\n\t`ad` bumps your channel to the top of the list.\n\n**IMPORTANT**: The `^ad` command can be used every 10 seconds!\n**JOIN:** [*official discord]* https://discord.gg/4T22QKn")
@@ -53,6 +56,19 @@ bot.on("message", async message => {
     message.author.send(`**AdBot:**\n\n\tRunning on: ${bot.guilds.size} servers.\n\n\tWatching: ${bot.users.size} online users.`)
     return message.channel.send("I DMed you my info!")
   }
+  if (message.content === '^custom-ad') {
+    let adschannel = message.guild.channels.find(`name`, "ads");
+    if(!adschannel) return message.channel.send("The bot is not properly set up! Please type `^test`.");
+    if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("No. Why would I do this for you? I have a **Admin only** policy.");
+    if (chratis_talked_users.has(message.author.id)) return message.reply("You have to wait before using this command again.\n*[Only 1 server can be advertised every 10 seconds.]*");
+    const sayMessage = args.join(" ");
+    message.channel.send("Your server has been advirtised!")
+    bot.channels.filter(c => c.name === 'ads').forEach(channel => channel.send(`**-----------------------------------------------------------**\n\n${sayMessage}\n\n**-----------------------------------------------------------**\n[Type \`^help\` for help and a link to join the official server!]`));
+    chratis_talked_users.add(message.author.id);
+    setTimeout(() => {
+      chratis_talked_users.delete(message.author.id);
+    }, chratis_cooldown_time * 1000);
+  } 
 });
 
 //Ik5KSLzA6C
