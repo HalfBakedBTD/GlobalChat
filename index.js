ef83c24225b4587341c3c7e2b4244dee95c4eddc
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-03-11 20:27:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bot = new Discord.Client({disableEveryone: true});
 const chratis_cooldown_time = 28;
 const chratis_talked_users = new Set();
 
-const button_cooldown_time = 10;
+const button_cooldown_time = 120;
 const button_talked_users = new Set();
 
 bot.on("ready", async () => {
